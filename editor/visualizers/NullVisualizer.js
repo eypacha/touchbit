f993d7ca2c1e862f463d0dce533b029b13bd6e86
Ey Pacha! <pachaguionbajo@gmail.com>
2024-01-23 19:22:54 -0300

    cleaning files

--- editor/visualizers/NullVisualizer.js
+++
@@ -1,7 +0,0 @@
-import Visualizer from './Visualizer.js';
-
-export default class NullVisualizer extends Visualizer {
-  constructor(canvas) {
-    super(canvas);
-  }
-}
--- editor/visualizers/effects/effect-utils.js
+++
@@ -1,8 +0,0 @@
-import * as twgl from '../../../js/twgl-full.module.js';
-
-export function drawEffect(gl, programInfo, bufferInfo, uniforms, commonUniforms, primitive) {
-  gl.useProgram(programInfo.program);
-  twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
-  twgl.setUniforms(programInfo, commonUniforms, uniforms);
-  twgl.drawBufferInfo(gl, bufferInfo, primitive);
-}
