96c6972ee32e47a06333fca4bc512fcf1d3fab7a
Ey Pacha! <pachaguionbajo@gmail.com>
2024-02-12 03:49:24 -0300

    condicional chaning para evitar error

--- js/ByteBeatNode.js
+++ js/ByteBeatNode.js
@@ -53,7 +53,7 @@ class BeatWorkletProcessor extends AudioWorkletProcessor {
   }
 
   process(inputs, outputs, parameters) {
-    this.byteBeat.process(outputs[0][0].length, outputs[0][0], outputs[0][1]);
+    this.byteBeat.process(outputs[0][0]?.length, outputs[0][0], outputs[0][1]);
     return true;
   }
 }
