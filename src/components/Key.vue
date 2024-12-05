e8452e387e11e6d666938c74337fb13c73b36555
Ey Pacha! <pachaguionbajo@gmail.com>
2024-12-04 22:17:43 -0300

    feat(ui): add props to Key component

--- src/components/Key.vue
+++ src/components/Key.vue
@@ -1,10 +1,24 @@
 <template>
-  <Button variant="outline" class="col-span-2 h-13 font-bold">
-    <slot />
+  <Button
+    variant="outline"
+    class="h-10 font-bold border-[1px] border-b-4 active:border-b-[1px] hover:bg-transparent select-none"
+    :class="`border-${color} active:bg-${color}/20`"
+  >
+    <span :class="`text-${color}`">
+      <slot />
+    </span>
   </Button>
 </template>
 
 <script setup>
 import { Button } from '@/components/ui/button'
+import { defineProps } from 'vue'
 
+const props = defineProps({
+  color: {
+    type: String,
+    required: false,
+    default: 'operator'
+  }
+})
 </script>
