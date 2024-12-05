7b4531df86909faaa4e885f0ed3df2f3f62dfb39
Ey Pacha! <pachaguionbajo@gmail.com>
2024-12-04 22:21:34 -0300

    feat(component): convert mockup to functional Keyboard component

--- src/components/Keyboard.vue
+++ src/components/Keyboard.vue
@@ -1,56 +1,69 @@
 <template>
- <div class="w-full h-full grid grid-cols-12 gap-2">
-
-  <Key class="col-span-3 text-fuchsia-600">UNDO</Key>
-  <Key class="col-span-3 text-fuchsia-600">REDO</Key>
-
-  <Key class="text-green-500">STACK</Key>
-  <Key class="text-green-500">Math</Key>
-  <Key class="text-green-500">?</Key>
-
-  <Key class="text-yellow-500">7</Key>
-  <Key class="text-yellow-500">8</Key>
-  <Key class="text-yellow-500">9</Key>
-
-  <Key class="text-white">*</Key>
-  <Key class="text-white">/</Key>
-  <Key class="text-white">&</Key>
-
-  <Key class="text-yellow-500">4</Key>
-  <Key class="text-yellow-500">5</Key>
-  <Key class="text-yellow-500">6</Key>
-
-  <Key class="text-white">+</Key>
-  <Key class="text-white">-</Key>
-  <Key class="text-white">|</Key>
-
-  <Key class="text-yellow-500">1</Key>
-  <Key class="text-yellow-500">2</Key>
-  <Key class="text-yellow-500">3</Key>
-
-  <Key class="text-white">%</Key>
-  <Key class="text-white">~</Key>
-  <Key class="text-white">^</Key>
-
-  <Key class="text-yellow-500">.</Key>
-  <Key class="text-yellow-500">0</Key>
-  <Key class="text-yellow-500">⌫</Key>
-
-  <Key class="text-cyan-500">t</Key>
-  <Key class="text-white">&laquo;</Key>
-  <Key class="text-white">&raquo;</Key>
-
-  <Key class="text-fuchsia-600">HOLD</Key>
-  <Key class="text-fuchsia-600">INS</Key>
-  <Key class="text-fuchsia-600">DEL</Key>
-  
-  <Key class="col-span-3 text-fuchsia-600 -scale-100">➝</Key>
-  <Key class="col-span-3 text-fuchsia-600">➝</Key>
-</div>
-
+  <div class="w-full h-full grid grid-cols-12 gap-2">
+    <Key v-for="(key, index) in layout" :key="index" :color="key.color ?? key.type" :class="getColSpan(key.width)"
+    :disabled="key.disabled" @click="keyPressed(key.type, key.data)">
+      <ChevronsLeft v-if="key.data === '<<'" />
+      <ChevronsRight v-else-if="key.data === '>>'" />
+      <Delete v-else-if="key.data === 'BCKS'"/>
+      <BetweenHorizontalEnd v-else-if="key.data === 'STACK'" stroke="hsl(var(--number))"/>
+      <Pi v-else-if="key.data === 'FUNC'" stroke="hsl(var(--number))"/>
+      <Share v-else-if="key.data === 'SHARE'" stroke="hsl(var(--number))"/>
+      <MoveLeft v-else-if="key.data === 'LEFT'" />
+      <MoveRight v-else-if="key.data === 'RIGHT'"/>
+      <Delete v-else-if="key.data === 'BCKS'" />
+      <template v-else> {{ key.data }} </template>
+    </Key>
+  </div>
 </template>
 
 <script setup>
-import Key from '@/components/Key.vue'
 
+import { useMainStore } from '@/stores/main'
+import Key from '@/components/Key.vue'
+import { Delete, MoveLeft, MoveRight, ChevronsLeft, ChevronsRight, Share, Pi, BetweenHorizontalEnd} from 'lucide-vue-next';
+
+const store = useMainStore()
+
+const layout = [
+  { type: 'action', data: 'UNDO', width: 3, disabled: true },
+  { type: 'action', data: 'REDO', width: 3 , disabled: true},
+  { type: 'action', data: 'STACK', color: 'time', disabled: true},
+  { type: 'action', data: 'FUNC', color: 'time' , disabled: true},
+  { type: 'action', data: 'SHARE', color: 'time', disabled: true},
+  { type: 'number', data: 7 },
+  { type: 'number', data: 8 },
+  { type: 'number', data: 9 },
+  { type: 'operator', data: '*' },
+  { type: 'operator', data: '/' },
+  { type: 'operator', data: '&' },
+  { type: 'number', data: 4 },
+  { type: 'number', data: 5 },
+  { type: 'number', data: 6 },
+  { type: 'operator', data: '+' },
+  { type: 'operator', data: '-' },
+  { type: 'operator', data: '|' },
+  { type: 'number', data: 1 },
+  { type: 'number', data: 2 },
+  { type: 'number', data: 3 },
+  { type: 'operator', data: '%' },
+  { type: 'operator', data: '~' },
+  { type: 'operator', data: '^' },
+  { type: 'number', data: '.' },
+  { type: 'number', data: 0 },
+  { type: 'action', data: 'BCKS', color: 'error' },
+  { type: 'time', data: 't' },
+  { type: 'operator', data: '<<' },
+  { type: 'operator', data: '>>' },
+  { type: 'action', data: 'HOLD' , disabled: true },
+  { type: 'action', data: 'INS', disabled: true },
+  { type: 'action', data: 'DEL', color: 'error' },
+  { type: 'action', data: 'LEFT', width: 3 },
+  { type: 'action', data: 'RIGHT', width: 3 }
+]
+
+const keyPressed = (type, data) => {
+  store.keyPressed(type, data)
+}
+
+const getColSpan = (width) => `col-span-${width ?? 2}`
 </script>
