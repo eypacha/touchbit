ce28a17e8a4495126fcdbb9f79b0aa225be55f16
Ey Pacha! <pachaguionbajo@gmail.com>
2025-04-25 14:43:00 -0300

    feat(audio): refactor volume controls and improve layout in AudioEffects and AudioSettings components

--- src/components/AudioEffects.vue
+++ src/components/AudioEffects.vue
@@ -1,10 +1,28 @@
 <template>
-  <div class="content">
-    Audio Effects
+  <div class="content min-w-[250px]">
+     <div class="block mb-6">
+          <Label>
+            Gain
+            <Slider v-model="volumeValue" class="my-2" :max="100" @update:modelValue="setVolume" @touchstart.stop />
+          </Label>
+        </div>
     
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
+import { useMainStore } from '@/stores/main';
+import { Label } from '@/components/ui/label';
+import { Slider } from '@/components/ui/slider';
+const volumeValue = ref([80])
+
+const store = useMainStore();
+function setVolume() {
+  const linearVolume = volumeValue.value[0] / 100;
+  const logVolume = Math.pow(linearVolume, 2);
+  store.setVolume(logVolume);
+}
 
 </script>
--- src/components/AudioSettings.vue
+++ src/components/AudioSettings.vue
@@ -1,24 +1,19 @@
 <template>
   <div class="content min-w-[250px]">
-    <div class="mb-6 block">
-      <Label>
-        Volume
-        <Slider v-model="volumeValue" class="my-2" :max="100" @update:modelValue="setVolume" @touchstart.stop />
-      </Label>
-    </div>
-    <div class="mb-5 flex justify-between items-center gap-2">
+    <div class="flex items-center justify-between gap-2 mb-5">
       <Label :for="'sampleRateLabel'">Sample rate</Label>
       <Number ref="sampleRateLabel"  class="w-24" v-model="selectedSampleRate" :step="10" @update:modelValue="setSampleRate"
         @touchstart="updatingSampleRate = true" @touchend="updatingSampleRate = false" />
     </div>
-    <div class="mb-5 flex justify-between items-center gap-2">
+    <div class="flex items-center justify-between gap-2 mb-5">
       <Label :for="'bpmLabel'">BPM</Label>
-      <Key variant="outline" class="border-gray ml-8" @click="tapTempo">
+      <Key variant="outline" class="ml-8 border-gray" @click="tapTempo">
         <small>tap</small>
       </Key>
       <Number ref="bpmLabel" class="w-24" v-model="selectedBPM" @update:modelValue="setBPM" @touchstart="updatingBPM = true"
         @touchend="updatingBPM = false" />
     </div>
+    <Key @click="caclB">C</Key>
     <div>
   </div>
   </div>
@@ -27,38 +22,38 @@
 
 <script setup>
 
-import { ref, watch} from 'vue';
+import { ref, watch } from 'vue';
 import { useMainStore } from '@/stores/main';
 
 import { calculateSampleRate, calculateBPM } from '@/lib/utils';
 import { useTapTempo } from '@/composables/useTapTempo';
 
-import { Slider } from '@/components/ui/slider';
 import Key from '@/components/Key.vue'
 import { Label } from '@/components/ui/label';
 import { Number } from '@/components/ui/Number';
 
+
 const store = useMainStore();
 
 const selectedBPM = ref(117)
-const volumeValue = ref([80])
 const selectedSampleRate = ref(8000)
 const updatingSampleRate = ref(false)
 const updatingBPM = ref(false)
 
 const { tempo, tapTempo } = useTapTempo();
 
-function setVolume() {
-  const linearVolume = volumeValue.value[0] / 100;
-  const logVolume = Math.pow(linearVolume, 2);
-  store.setVolume(logVolume);
-}
 
 watch(tempo, (newTempo) => {
   selectedBPM.value = newTempo;
   setBPM();
 });
 
+
+function caclB() {
+  console.log('caclB');
+  console.log(calculateBPM(selectedSampleRate.value, store.stack[1].data, store.stack[2].data));
+  console.log(calculateSampleRate(selectedBPM.value, store.stack[1].data, store.stack[2].data));
+}
 function setSampleRate() {
   if (updatingBPM.value) return;
   console.log('setSampleRate');
