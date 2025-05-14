f99cb42543e1458994e862d2c610e433ffe63378
Ey Pacha! <pachaguionbajo@gmail.com>
2025-05-14 19:05:08 -0300

    refactor(EditorControls): improve button opacity and structure for better clarity

--- src/components/number-editor/EditorControls.vue
+++ src/components/number-editor/EditorControls.vue
@@ -4,24 +4,47 @@
       color="action"
       class="text-xs"
       @click="$emit('toggleMode')">
-      <span :class="{ 'opacity-50': mode !== NUMBER_MODE.BINARY }">bin</span>
+      <span :class="{ 'opacity-40': mode !== NUMBER_MODE.BINARY }">bin</span>
       <span> </span>
-      <span :class="{ 'opacity-50': mode !== NUMBER_MODE.DECIMAL }">dec</span>
+      <span :class="{ 'opacity-40': mode !== NUMBER_MODE.DECIMAL }">dec</span>
     </Key>
     <Key color="action" @click="$emit('undo')">UNDO</Key>
     <Key color="action" @click="$emit('randomize')">RND</Key>
-    <Key color="action" @click="$emit('max')">MAX</Key>
-    <Key color="action" @click="$emit('min')">MIN</Key>
+    <Key color="action" @click="$emit('max')">
+        <MoveUp/>
+    </Key>
+    <Key color="action" @click="$emit('min')">
+        <MoveDown/>
+    </Key>
 
     <Key color="action" @click="$emit('invert')">~</Key>
-    <Key color="action" @click="$emit('leftShift')">&lt;&lt;</Key>
-    <Key color="action" @click="$emit('rightShift')">&gt;&gt;</Key>
-    <Key color="action" @click="$emit('circularLeftShift')">c&lt;&lt;</Key>
-    <Key color="action" @click="$emit('circularRightShift')">c&gt;&gt;</Key>
+    <Key color="action" @click="$emit('leftShift')">
+        <ChevronsLeft/>
+    </Key>
+    <Key color="action" @click="$emit('rightShift')">
+        <ChevronsRight/>
+    </Key>
+    <Key color="action" @click="$emit('circularLeftShift')">
+        <div class="flex items-center justify-center">
+            <div>c</div><ChevronsLeft/>
+        </div>
+    </Key>
+    <Key color="action" @click="$emit('circularRightShift')">
+        <div class="flex items-center justify-center">
+            <div>c</div><ChevronsRight/>
+        </div>
+    </Key>
   </div>
 </template>
 
 <script setup>
+import {
+  MoveUp,
+  MoveDown,
+  ChevronsLeft,
+  ChevronsRight,
+} from 'lucide-vue-next';
+
 import Key from '@/components/Key.vue';
 import { NUMBER_MODE } from '@/constants/index.js';
 
